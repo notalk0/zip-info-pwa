631489c3594d00e43b2fbee33232b04d86af5e45
Reina Liew <notalk_0@hotmail.com>
2019-06-10 01:53:03 +0800

    Initial Done

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipInfo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding" fullscreen="true">
+      <ZipSearch v-on:get-zip="getZipInfo"/>
+      <ZipInfo v-bind:info="info"/>
+      <ClearInfo v-bind:info="info" v-on:clear-info="clearInfo"/>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
+import ClearInfo from "../components/ClearInfo";
+
+export default {
+  name: "home",
+  components: { ZipSearch, ZipInfo, ClearInfo },
+  data() {
+    return { info: null };
+  },
+  methods: {
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
+      if (res.status == 404) {
+        this.showAlert();
+      }
+      this.info = res;
+    },
+    clearInfo() {
+      this.info = null;
+    },
+    showAlert(err) {
+      return this.$ionic.alertController
+        .create({
+          header: "Not Valid",
+          message: err,
+          buttons: ["Try again"]
+        })
+        .then(a => a.present());
+    }
+  }
+};
+</script>
